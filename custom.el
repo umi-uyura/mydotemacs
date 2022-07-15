ad37fc297c46a3bc40ffdc0d4670b1f05663506d
Umi Uyura <umiuyura@gmail.com>
2022-07-15 17:13:00 +0900

    markdown-modeのコードブロックにも指定フォントを適用する

--- custom.el
+++ custom.el
@@ -47,4 +47,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "PlemolJP HS" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "PlemolJP HS" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal))))
+ '(markdown-code-face ((t (:inherit nil)))))
