5f7fbe4514f07e0b47329d4e65cfcdffb987a077
Umi Uyura <umiuyura@gmail.com>
2025-02-13 15:16:02 +0900

    magitのキーバインド見直し

--- init.el
+++ init.el
@@ -291,7 +291,9 @@
 ;;; @ magit                                                         ;;;
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
-(global-set-key (kbd "C-c m s") 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch)
+(global-set-key (kbd "C-x C-g") 'magit-file-dispatch)
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
